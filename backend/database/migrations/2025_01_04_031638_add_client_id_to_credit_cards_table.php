ac874cae58758c932a913681572fb665880b1a9d
Douglas Oliveira <douglasgama0@gmail.com>
2025-01-06 12:13:29 -0300

    fix: ajuste migration

--- backend/database/migrations/2025_01_04_031638_add_client_id_to_credit_cards_table.php
+++ backend/database/migrations/2025_01_04_031638_add_client_id_to_credit_cards_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('credit_cards', function (Blueprint $table) {
-            $table->foreignId('client_id')->nullable()->change();
+            $table->foreignId('client_id')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('credit_cards', function (Blueprint $table) {
-            $table->foreignId('client_id')->nullable()->change();
+            $table->dropColumn('client_id');
         });
     }
 };
